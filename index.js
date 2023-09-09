b8ce3c0512301b83aa3f05644180bfb99c1d4989
frey808 <kaifrazier413@gmail.com>
2023-09-08 23:28:32 -0400

    fixed database-js garbage

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-import 'dotenv/config'
-import { connect } from '@planetscale/database'
-
-const config = {
-  host: env.DATABASE_HOST,
-  username: env.DATABASE_USERNAME,
-  password: env.DATABASE_PASSWORD
-}
-
-// const conn = connect(config)
-// const results = await conn.execute('select 1 from dual where 1=?', [1])
-
 function openNav(){
     document.getElementById("nav").style.display = "inline";
     document.getElementById("opennav").style.display = "none";
@@ -24,37 +12,8 @@ function swap(){
     //swap view
 }
 
-// function exec_sql_file(path){
-//     conn = connect(config)
-//     results = await conn.execute('select 1 from dual where 1=?', [1])
-//     cur = conn.cursor()
-//     with open(path, 'r') as file:
-//         cur.execute(file.read())
-//     conn.commit()
-//     conn.close()
-// }
-
-function createTables(){
-    sqlCommit("CREATE TABLE Persons (id SERIAL PRIMARY KEY, name VARCHAR(30), image INT, description VARCHAR(255));");
-}
-
-function sqlCommit(sql, args={}){
-    conn = connect();
-    conn.execute(sql, args);
-    conn.commit();
-    conn.close();
-    return result;
-}
-
-function sqlGetAll(sql, args={}){
-    conn = connect();
-    result = conn.query(sql, args);
-    conn.close();
-    return result;
-}
-
 function getData(){
-    //load
+    //load data using http req to python server
 }
 
 function createItem(){
